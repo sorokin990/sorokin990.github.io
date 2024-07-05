7e5c726b106491dbe2e71386a949041b72e72ac0
Sorokin <23767990@qq.com>
2024-07-05 16:49:54 +0800

    Create tongji.js

---
+++ docs/js/tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?038d913d014477a141ed80b6081fc27a";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
